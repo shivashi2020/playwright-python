dc1b9577ea0f2f603f3bcf6cb5e277a56c04e112
shiva <shivananda44@gmail.com>
2025-01-02 22:35:14 +0530

    prg01 added

---
+++ samplePrg.py
@@ -0,0 +1,12 @@
+# Given an array of integers, find the sum of its elements.
+
+def sumofInt(listvalue):
+    sum = 0
+    for value in listvalue:
+        sum = sum + value
+    return sum
+
+print(sumofInt({12, 3, 4, 15}))
+
+
+
